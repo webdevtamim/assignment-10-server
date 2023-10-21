58033faa98d6f469785c009b558d5265f70e78e9
webdevtamim <tamimahmed12.bd@gmail.com>
2023-10-21 22:36:42 +0600

    update cart

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-// const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -50,6 +49,13 @@ async function run() {
             const cursor = cartCollection.find()
             const cartData = await cursor.toArray();
             res.send(cartData);
+        })
+
+        app.get('/cart/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await cartCollection.findOne(query);
+            res.send(result);
         })
 
         app.post('/cart', async (req, res) => {
